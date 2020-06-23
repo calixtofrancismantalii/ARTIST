21f1d126a75b46b7b5679fa52dbd57168278c44b
Cyries <cyriesmadrid@gmail.com>
2020-06-23 14:23:32 +0800

    inserted contact.js

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+function myMap() {
+var mapProp = {
+    center:new google.maps.LatLng(14.599512,120.984222),
+    zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
